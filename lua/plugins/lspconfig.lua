0110ef93edbdf9f4b981e974b232121f8a03b349
c <corne.spek15@gmail.com>
2024-03-17 14:32:35 +0100

    add rounded borders for lsp hover

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,20 +1,6 @@
--- local border = {
--- 	{ "🭽", "FloatBorder" },
--- 	{ "▔", "FloatBorder" },
--- 	{ "🭾", "FloatBorder" },
--- 	{ "▕", "FloatBorder" },
--- 	{ "🭿", "FloatBorder" },
--- 	{ "▁", "FloatBorder" },
--- 	{ "🭼", "FloatBorder" },
--- 	{ "▏", "FloatBorder" },
--- }
---
--- local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
--- function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
---   opts = opts or {}
---   opts.border = opts.border or border
---   return orig_util_open_floating_preview(contents, syntax, opts, ...)
--- end
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded", silent = true })
+vim.lsp.handlers["textDocument/signatureHelp"] =
+    vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded", silent = true })
 
 return {
 	"neovim/nvim-lspconfig",
