d834163576ec94d383d1d0d227637caeefd48275
c <corne.spek15@gmail.com>
2024-03-23 21:29:34 +0100

    change colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,15 @@
 return {
-	"catppuccin/nvim",
+	-- "catppuccin/nvim",
+	"sainnhe/gruvbox-material",
 	lazy = false,
 	priority = 1000,
+	init = function()
+		vim.g.gruvbox_material_background = "hard"
+		vim.g.gruvbox_material_foreground = "mix"
+		vim.g.gruvbox_material_dim_inactive_windows = 0
+		vim.g.gruvbox_material_float_style = "dim"
+	end,
 	config = function()
-		vim.cmd.colorscheme("catppuccin")
+		vim.cmd.colorscheme("gruvbox-material")
 	end,
 }
